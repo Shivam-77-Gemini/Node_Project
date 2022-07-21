62d50fbac3c24323e073f43ed82701873d31f5ed
Shivam-77-Gemini <Shivam.Tiwari@Geminisolutions.com>
2022-07-21 16:00:53 +0530

    Add

--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,14 @@
 // console.log(Person.age);
 console.log(__dirname,__filename);
 const path=require('path');
+
+//File and directory location
 console.log(path.basename(__filename));
 console.log(path.basename(__dirname));
+
+//File Extension
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
+
+//Create Path Object
+console.log(path.parse(__filename));
